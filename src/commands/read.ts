b0dc398dbeef938fd0ca3edea2844dd0ed4e8cfc
Owen Farrell <owen.farrell@gmail.com>
2020-05-12 14:37:05 -0400

    Removed value type icon from status bar text
    Signed-off-by: Owen Farrell <owen.farrell@gmail.com>

--- src/commands/read.ts
+++ src/commands/read.ts
@@ -5,6 +5,7 @@ import * as nv from 'node-vault';
 import * as vscode from 'vscode';
 
 interface SecretQuickPickItem extends vscode.QuickPickItem {
+    key: string
     secretValue: string;
 }
 
@@ -15,16 +16,18 @@ export default async function(client: nv.client, path: string): Promise<void> {
     const flattenedData: any = flat.flatten(result.data);
     // Create a list of quick pick items for the the flattened data
     const items: SecretQuickPickItem[] = [];
-    // For each mount point
+    // For each field
     for (const key in flattenedData) {
+        // Get the value of the field
+        const value = flattenedData[key];
         // Create a new quick pick item from the key/value pair
-        items.push({ label: `$(symbol-${typeof flattenedData[key]}) ${key}`, secretValue: flattenedData[key] });
+        items.push({ label: `$(symbol-${typeof value}) ${key}`, key: key, secretValue: value });
     }
     // If only one quick pick item exists, automatically select it, otherwise prompt for the item to pick
     const selectedItem = items.length === 1 ? items[0] : await vscode.window.showQuickPick(items, { placeHolder: 'Select an attribute' });
     // If an item was (automatically or manually) selected
     if (selectedItem) {
         // Clip the value of the selected item
-        vscode.window.vault.clip(selectedItem.label, selectedItem.secretValue);
+        vscode.window.vault.clip(selectedItem.key, selectedItem.secretValue);
     }
 }
