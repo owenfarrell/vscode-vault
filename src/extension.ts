a6ff3cda289694097720b110bb1230f3f6a36771
Owen Farrell <owen.farrell@gmail.com>
2020-06-23 15:52:45 -0400

    Extracted comand callback functions
    Signed-off-by: Owen Farrell <owen.farrell@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -27,25 +27,49 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
 
+    const browseFn = (treeItem: view.VaultServerTreeItem) => treeItem.browse()
+        .then(() => vaultTreeDataProvider.refresh(treeItem))
+        .catch((err: Error) => vaultWindow.logError(`Unable to browse Vault path (${err.message})`));
+
+    const connectFn = () => vaultTreeDataProvider.connect()
+        .catch((err: Error) => vaultWindow.logError(`Unable to connect to Vault (${err.message})`));
+
+    const deleteFn = (treeItem: view.VaultSecretTreeItem) => treeItem.delete()
+        .then(() => vaultTreeDataProvider.refresh(treeItem.parent))
+        .catch((err: Error) => vaultWindow.logError(`Unable to delete Vault path (${err.message})`));
+
+    const disconnectFn = (treeItem: view.VaultServerTreeItem) => vaultTreeDataProvider.disconnect(treeItem)
+        .catch((err: Error) => vaultWindow.logError(`Unable to connect to Vault (${err.message})`));
+
+    const listFn = (treeItem: view.VaultTreeItem) => vaultTreeDataProvider.refresh(treeItem)
+        .catch((err: Error) => vaultWindow.logError(`Unable to list Vault path (${err.message})`));
+
+    const readFn = (treeItem: view.VaultSecretTreeItem) => treeItem.read()
+        .catch((err: Error) => vaultWindow.logError(`Unable to read Vault path (${err.message})`));
+
+    const writeFn = (treeItem: view.VaultPathTreeItem | view.VaultSecretTreeItem) => treeItem.write()
+        .then((requiresRefresh: boolean) => requiresRefresh === true && vaultTreeDataProvider.refresh(treeItem))
+        .catch((err: Error) => vaultWindow.logError(`Unable to write Vault path (${err.message})`));
+
     // Push disposables on to context
     context.subscriptions.push(
         vaultWindow,
         eventListener,
         vscode.window.createTreeView('vaultSecrets', { treeDataProvider: vaultTreeDataProvider }),
         // Subscribe to "vault.browse" events
-        vscode.commands.registerCommand('vault.browse', (treeItem: view.VaultServerTreeItem) => treeItem.browse().then(() => vaultTreeDataProvider.refresh(treeItem)).catch((err: Error) => vaultWindow.logError(`Unable to browse Vault path (${err.message})`))),
+        vscode.commands.registerCommand('vault.browse', browseFn),
         // Subscribe to "vault.connect" events
-        vscode.commands.registerCommand('vault.connect', () => vaultTreeDataProvider.connect().catch((err: Error) => vaultWindow.logError(`Unable to connect to Vault (${err.message})`))),
-        // Subscribe to "vault.disconnect" events
-        vscode.commands.registerCommand('vault.disconnect', (treeItem: view.VaultServerTreeItem) => vaultTreeDataProvider.disconnect(treeItem).catch((err: Error) => vaultWindow.logError(`Unable to connect to Vault (${err.message})`))),
+        vscode.commands.registerCommand('vault.connect', connectFn),
         // Subscribe to "vault.delete" events
-        vscode.commands.registerCommand('vault.delete', (treeItem: view.VaultSecretTreeItem) => treeItem.delete().then(() => vaultTreeDataProvider.refresh(vaultTreeDataProvider.getParent(treeItem))).catch((err: Error) => vaultWindow.logError(`Unable to delete Vault path (${err.message})`))),
+        vscode.commands.registerCommand('vault.delete', deleteFn),
+        // Subscribe to "vault.disconnect" events
+        vscode.commands.registerCommand('vault.disconnect', disconnectFn),
         // Subscribe to "vault.list" events
-        vscode.commands.registerCommand('vault.list', (treeItem: view.VaultTreeItem) => vaultTreeDataProvider.refresh(treeItem).catch((err: Error) => vaultWindow.logError(`Unable to list Vault path (${err.message})`))),
+        vscode.commands.registerCommand('vault.list', listFn),
         // Subscribe to "vault.read" events
-        vscode.commands.registerCommand('vault.read', (treeItem: view.VaultSecretTreeItem) => treeItem.read().catch((err: Error) => vaultWindow.logError(`Unable to read Vault path (${err.message})`))),
+        vscode.commands.registerCommand('vault.read', readFn),
         // Subscribe to "vault.write" events
-        vscode.commands.registerCommand('vault.write', (treeItem: view.VaultPathTreeItem | view.VaultSecretTreeItem) => treeItem.write().then((requiresRefresh: boolean) => requiresRefresh === true && vaultTreeDataProvider.refresh(treeItem)).catch((err: Error) => vaultWindow.logError(`Unable to write Vault path (${err.message})`)))
+        vscode.commands.registerCommand('vault.write', writeFn)
     );
 }
 
