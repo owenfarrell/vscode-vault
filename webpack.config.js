f52924fbaeff75ca5f89b6b5ad6bb71b714ec31f
Owen Farrell <owen.farrell@gmail.com>
2020-06-24 01:04:49 -0400

    Check for circular dependencies at build time
    Signed-off-by: Owen Farrell <owen.farrell@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-//@ts-check
+// @ts-check
 
 'use strict';
 
+const CircularDependencyPlugin = require('circular-dependency-plugin');
 const path = require('path');
 
-/**@type {import('webpack').Configuration}*/
+/** @type {import('webpack').Configuration} */
 const config = {
     target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
 
@@ -39,6 +40,19 @@ const config = {
     },
     node: {
         __dirname: false
-    }
+    },
+    plugins: [
+        new CircularDependencyPlugin({
+            // exclude detection of files based on a RegExp
+            exclude: /a\.js|node_modules/,
+            // add errors to webpack instead of warnings
+            failOnError: true,
+            // allow import cycles that include an asyncronous import,
+            // e.g. via import(/* webpackMode: "weak" */ './file.js')
+            allowAsyncCycles: false,
+            // set the current working directory for displaying module paths
+            cwd: process.cwd()
+        })
+    ]
 };
 module.exports = config;
