c5d357d5a8eb0158a8564b76032802cb3f3ef5b9
Owen Farrell <owen.farrell@gmail.com>
2020-06-24 12:17:52 -0400

    Provide ability to opt out of saving connections
    Signed-off-by: Owen Farrell <owen.farrell@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -11,18 +11,64 @@ declare module 'vscode' {
     }
 }
 
+const CONFIGURATION_SECTION = 'vault';
+const GLOBAL_STATE_SESSIONS_KEY = 'sessions';
+
 export function activate(context: vscode.ExtensionContext) {
     const vaultWindow = vscode.window.vault = new model.VaultWindow();
 
+    const clearSavedSessions = () => context.globalState.update(GLOBAL_STATE_SESSIONS_KEY, undefined);
+
+    let sessionList: model.VaultSession[];
+    const loadSavedSessions = () => {
+        try {
+            // Get the list of saved configurations from the global state
+            const savedConfigList = context.globalState.get<model.VaultConnectionConfig[]>(GLOBAL_STATE_SESSIONS_KEY);
+            // If a list of saved configurations exists
+            if (savedConfigList) {
+                // For each configuration
+                sessionList = savedConfigList.map(connectionConfig => {
+                    // Create a new session
+                    const session = new model.VaultSession(connectionConfig);
+                    // For each mount point
+                    for (const entry of connectionConfig.mountPoints) {
+                        // Mount the mount point
+                        session.mount(entry[0], entry[1]);
+                    }
+                    // Return the new (disconnected) session
+                    return session;
+                });
+            }
+        }
+        catch (err) {
+            // Log an error
+            vaultWindow.logError(`Unable to load session configuration from global state (${err.message})`);
+            // Clear the list of saved configurations (if any) from disk
+            clearSavedSessions();
+        }
+    };
+
+    let saveConfigurations: boolean;
     const loadConfig = () => {
         // Get the Vault extension configuration
-        const configuration = vscode.workspace.getConfiguration('vault');
+        const configuration = vscode.workspace.getConfiguration(CONFIGURATION_SECTION);
         // Update the clipboard timeout based on the clipboardTimeout value from the configuration
         vaultWindow.clipboardTimeout = configuration.get<number>('clipboardTimeout') * 1000;
         // Clear the list of trusted authorities
         config.TRUSTED_AUTHORITIES.length = 0;
         // Get the trustedAuthorities value from the configuration
-        config.TRUSTED_AUTHORITIES.concat(configuration.get<string[]>('trustedAuthorities'));
+        config.TRUSTED_AUTHORITIES.push(...configuration.get<string[]>('trustedAuthorities'));
+        // Get the saveSessionConfiguration value from the configuration
+        saveConfigurations = configuration.get<boolean>('saveConfigurations');
+        // If configuration saving is enabled
+        if (saveConfigurations) {
+            // Load the saved sessions
+            loadSavedSessions();
+        }
+        else {
+            // Clear the list of saved configurations (if any) from disk
+            clearSavedSessions();
+        }
     };
 
     // Load the configuration to start
@@ -30,33 +76,21 @@ export function activate(context: vscode.ExtensionContext) {
     // Subscribe to configuration event changes
     const eventListener: vscode.Disposable = vscode.workspace.onDidChangeConfiguration((event: vscode.ConfigurationChangeEvent) => {
         // If the Vault extension configuration changed
-        if (event.affectsConfiguration('vault')) {
+        if (event.affectsConfiguration(CONFIGURATION_SECTION)) {
             // (Re)Load the configuration
             loadConfig();
         }
     });
 
-    let sessionList: model.VaultSession[];
-    try {
-        const savedConfigList = context.globalState.get<model.VaultConnectionConfig[]>('sessions');
-        if (savedConfigList) {
-            sessionList = savedConfigList.map(config => {
-                // TODO Implement error handling
-                const session = new model.VaultSession(config);
-                for (const entry of config.mountPoints) {
-                    session.mount(entry[0], entry[1]);
-                }
-                return session;
-            });
-        }
-    }
-    catch (err) {
-        vaultWindow.logError(`Unable to load session configuration from global state (${err.message})`);
-    }
-
     const vaultTreeDataProvider = new view.VaultTreeDataProvider(sessionList);
 
-    const saveSessionList = () => context.globalState.update('sessions', vaultTreeDataProvider.sessionList.map(element => element.config));
+    const saveSessionList = () => {
+        // If configuration saving is enabled
+        if (saveConfigurations === true) {
+            // Update the list of saved sessions
+            context.globalState.update(GLOBAL_STATE_SESSIONS_KEY, vaultTreeDataProvider.sessionList.map(element => element.config));
+        }
+    };
 
     const browseFn = (treeItem: view.VaultServerTreeItem) => treeItem.browse()
         .then(() => vaultTreeDataProvider.refresh(treeItem))
